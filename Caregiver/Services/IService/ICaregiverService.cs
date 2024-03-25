215f8d714d929383c5ca6f7655a84b9679ca18e9
Eyad <Eyad@DESKTOP-LTMLQ70>
2024-03-25 07:10:18 +0200

    .

---
+++ Caregiver/Services/IService/ICaregiverService.cs
@@ -0,0 +1,18 @@
+﻿using Caregiver.Dtos;
+using Caregiver.Dtos.UpdateDTOs;
+using Caregiver.Models;
+
+namespace Caregiver.Services.IService
+{
+	public interface ICaregiverService
+	{
+		 Task<IEnumerable<CaregiverCardDTO>> GetAllCurrentCaregiver();
+		Task<IEnumerable<CaregiverCardDTO>> GetAllCaregiverByType(string Role);
+		Task<CaregiverUser> GetCaregiverById(string id);
+		Task<CaregiverUpdateDTO> UpdateCaregiverAsync(string id, CaregiverUpdateDTO caregiverUpdate);
+		Task<bool> SoftDeleteCaregiver(string id);
+		//
+		//
+
+	}
+}
