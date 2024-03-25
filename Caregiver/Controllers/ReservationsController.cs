9f4af33461c2c4ffee55ad175a205c483935bc10
sohila <sohilaafify23@gmail.com>
2024-03-25 02:20:35 +0200

    new upload

--- Caregiver/Controllers/ReservationsController.cs
+++ Caregiver/Controllers/ReservationsController.cs
@@ -19,22 +19,15 @@ namespace Caregiver.Controllers
         private readonly IReservationsRepo reservationsRepo;
         private UserManager<User> _userManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
-       // private readonly ICaregiverRepo _dbCaregiver;
-        private readonly ICaregiverRepo _dbCaregiver;
         private readonly ICaregiverRepo _dbCaregiver;
 
 
-        public ReservationsController(ApplicationDBContext context, IReservationsRepo _reservationsRepo, UserManager<User> userManager, IHttpContextAccessor httpContextAccessor //ICaregiverRepo dbCaregiver
-            )
-        public ReservationsController(ApplicationDBContext context, IReservationsRepo _reservationsRepo, UserManager<User> userManager, IHttpContextAccessor httpContextAccessor, ICaregiverRepo dbCaregiver)
         public ReservationsController(ApplicationDBContext context, IReservationsRepo _reservationsRepo, UserManager<User> userManager, IHttpContextAccessor httpContextAccessor, ICaregiverRepo dbCaregiver)
         {
             reservationsRepo= _reservationsRepo;
             _context = context;
             _userManager = userManager;
             _httpContextAccessor = httpContextAccessor;
-           // _dbCaregiver = dbCaregiver;
-            _dbCaregiver = dbCaregiver;
             _dbCaregiver = dbCaregiver;
         }
 
@@ -102,24 +95,6 @@ namespace Caregiver.Controllers
                     EndDate = (DateTime)reservation.EndDate,
                     Photo= reservation.Caregiver.Photo,
                     Country=reservation.Caregiver.Country,
-                   // OrderId = reservation.OrderId,
-                   // Gender = reservation.Caregiver.Gender.ToString(),
-                   // Status = reservation.Status,
-                    //TotalPrice= reservation.totalPrice
-                    CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                    PatientEmailAddress = reservation.Patient.Email,
-                    PatientPhoneNumber = reservation.Patient.PhoneNumber,
-                    StartDate = (DateTime)reservation.StartDate,
-                    EndDate = (DateTime)reservation.EndDate,
-                    Photo= reservation.Caregiver.Photo,
-                    Country=reservation.Caregiver.Country,
-                    CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                    PatientEmailAddress = reservation.Patient.Email,
-                    PatientPhoneNumber = reservation.Patient.PhoneNumber,
-                    StartDate = (DateTime)reservation.StartDate,
-                    EndDate = (DateTime)reservation.EndDate,
-                    Photo= reservation.Caregiver.Photo,
-                    Country=reservation.Caregiver.Country,
                     OrderId = reservation.OrderId,
                     Gender = reservation.Caregiver.Gender,
                     Status = reservation.Status,
@@ -141,34 +116,8 @@ namespace Caregiver.Controllers
             /*
                      var reservation = await reservationsRepo.GetPatientReservationById(id);
 
-
-                     if (reservation == null)
-                     {
-                         return NotFound();
-                     }
-                     else
-                     {
-                         var dto = new ReservationDto
-                         {
-                             PatientId = reservation.PatientId,
-                             CaregiverFirstName = reservation.Caregiver.FirstName,
-                             CaregiverLastName = reservation.Caregiver.LastName,
-                             PatientFirstName = reservation.Patient.FirstName,
-                             PatientLastName = reservation.Patient.LastName,
-                             CaregiverEmailAddress = reservation.Caregiver.Email,
-                             CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                             OrderId = reservation.OrderId,
-                             Gender = reservation.Caregiver.Gender,
-                             Status = reservation.Status,
-                             TotalPrice = reservation.totalPrice
-                         };
-            /*
-                     var reservation = await reservationsRepo.GetPatientReservationById(id);
-
                          return Ok(dto);*/
 
-            var Patientreservation = await reservationsRepo.GetPatientReservationById(id);
-            var Caregivrerreservation = await reservationsRepo.GetCaregiverReservationById(id);
                      if (reservation == null)
                      {
                          return NotFound();
@@ -190,27 +139,6 @@ namespace Caregiver.Controllers
                              TotalPrice = reservation.totalPrice
                          };
 
-            if (Patientreservation == null && Caregivrerreservation == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = Patientreservation ?? Caregivrerreservation;
-
-            var dto = new ReservationDto
-            {
-                PatientId = reservation.PatientId,
-                CaregiverFirstName = reservation.Caregiver.FirstName,
-                CaregiverLastName = reservation.Caregiver.LastName,
-                PatientFirstName = reservation.Patient.FirstName,
-                PatientLastName = reservation.Patient.LastName,
-                CaregiverEmailAddress = reservation.Caregiver.Email,
-                CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                //OrderId = reservation.OrderId,
-                //Gender = reservation.Caregiver.Gender,
-                //Status = reservation.Status,
-                //TotalPrice = reservation.totalPrice
-            };
                          return Ok(dto);*/
 
             var Patientreservation = await reservationsRepo.GetPatientReservationById(id);
@@ -247,205 +175,6 @@ namespace Caregiver.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateReservationsAsync([FromForm] PostReservationDto  dto, [FromQuery] string CaregiverId)
         {
-            //var caregiver = await _dbCaregiver.GetAsync(a => a.Id == CaregiverId);
-           // var pricePerDay = caregiver.PricePerDay;
-            ReservationStatus reservationStatus = ReservationStatus.OnProgress;
-          //  var calculatedValue = CalculateTotalPrice(pricePerDay, dto.EndDate, dto.StartDate);
-            var loggedInUserId = _userManager.GetUserId(_httpContextAccessor.HttpContext.User);
-
-
-            //if (caregiver == null)
-            //{
-            //    return NotFound();
-            //}
-
-            var caregiverPatientReservation = new CaregiverPatientReservation
-            {
-            CaregiverId= CaregiverId,
-            PatientId=loggedInUserId,
-           // Status = reservationStatus,
-            StartDate = dto.StartDate,
-            EndDate = dto.EndDate,
-            //totalPrice = calculatedValue,
-            };
-
-            #region Email section
-            var r = "sohilaafify23@gmail.com";
-            var s = "Test subject";
-            var m = "Your Reservation is confirmed!";
-            await reservationsRepo.SendEmailAsync(r, s, m);
-            #endregion
-
-            await reservationsRepo.AddReservarion(caregiverPatientReservation);
-
-            return Ok(dto);
-        }
-
-        #endregion
-
-
-        #region  Confirm or reject or cancel reservation request
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateReservationStatusAsync(int id, [FromForm] ReservationStatusDto dto)
-        {
-            var reservation = await  reservationsRepo.GetReservationById(id);
-
-            if (reservation == null) return NotFound($"No reservation was found with ID: {id}");
-
-            if (dto.Status == ReservationStatus.Done)
-            {
-              //  reservation.Status = dto.Status;
-              //if(reservationsRepo.CheckReservationDatesExists(reservation.OrderId)) { return BadRequest(); }
-              //else { AddDatesToDatabase(reservation); }
-                   
-                
-            }
-            else if (dto.Status == ReservationStatus.CannotProceed || dto.Status == ReservationStatus.Cancelled)
-            {
-             //   reservation.Status = dto.Status;
-                await reservationsRepo.DeleteReservationStatus(id);
-            }
-            reservationsRepo.UpdateReservationStatus(reservation);
-            return Ok(reservation);
-        }
-        #endregion
-            return Ok(dto);
-        }
-        
-        #endregion
-
-        #region Reserve a caregiver
-        [HttpPost]
-        public async Task<IActionResult> CreateReservationsAsync([FromForm] PostReservationDto  dto, [FromQuery] string CaregiverId)
-        {
-            var caregiver = await _dbCaregiver.GetAsync(a => a.Id == CaregiverId);
-            var pricePerDay = caregiver.PricePerDay;
-            ReservationStatus reservationStatus = ReservationStatus.OnProgress;
-            var calculatedValue = CalculateTotalPrice(pricePerDay, dto.EndDate, dto.StartDate);
-            var loggedInUserId = _userManager.GetUserId(_httpContextAccessor.HttpContext.User);
-
-
-        #region Add dates to database
-        protected async void AddDatesToDatabase(CaregiverPatientReservation reservation)
-        {
-                     if (reservation == null)
-                     {
-                         return NotFound();
-                     }
-                     else
-                     {
-                         var dto = new ReservationDto
-                         {
-                             PatientId = reservation.PatientId,
-                             CaregiverFirstName = reservation.Caregiver.FirstName,
-                             CaregiverLastName = reservation.Caregiver.LastName,
-                             PatientFirstName = reservation.Patient.FirstName,
-                             PatientLastName = reservation.Patient.LastName,
-                             CaregiverEmailAddress = reservation.Caregiver.Email,
-                             CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                             OrderId = reservation.OrderId,
-                             Gender = reservation.Caregiver.Gender,
-                             Status = reservation.Status,
-                             TotalPrice = reservation.totalPrice
-                         };
-
-            for (DateTime date = reservation.StartDate; date <= reservation.EndDate; date = date.AddDays(1))
-            {
-                ReservationDates reservationDate = new ReservationDates
-                {
-                    //OrderId = reservation.OrderId,
-                    ReservationDate = date
-                };
-                await reservationsRepo.AddReservationDates(reservationDate);
-            }
-        }
-        #endregion
-            if (caregiver == null)
-            {
-                return NotFound();
-            }
-                         return Ok(dto);*/
-
-            var Patientreservation = await reservationsRepo.GetPatientReservationById(id);
-            var Caregivrerreservation = await reservationsRepo.GetCaregiverReservationById(id);
-
-            if (Patientreservation == null && Caregivrerreservation == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = Patientreservation ?? Caregivrerreservation;
-
-            var dto = new ReservationDto
-            {
-                PatientId = reservation.PatientId,
-                CaregiverFirstName = reservation.Caregiver.FirstName,
-                CaregiverLastName = reservation.Caregiver.LastName,
-                PatientFirstName = reservation.Patient.FirstName,
-                PatientLastName = reservation.Patient.LastName,
-                CaregiverEmailAddress = reservation.Caregiver.Email,
-                CaregiverPhoneNumber = reservation.Caregiver.PhoneNumber,
-                OrderId = reservation.OrderId,
-                Gender = reservation.Caregiver.Gender,
-                Status = reservation.Status,
-                TotalPrice = reservation.totalPrice
-            };
-
-        #region Calculate Total Price method
-        private int CalculateTotalPrice(int pricePerHour, DateTime EndDate, DateTime startDate)
-        {
-            TimeSpan difference = EndDate - startDate;
-            int totalDays = ((int)difference.TotalDays)+1;
-            var caregiverPatientReservation = new CaregiverPatientReservation
-            {
-            CaregiverId= CaregiverId,
-            PatientId=loggedInUserId,
-            Status = reservationStatus,
-            StartDate = dto.StartDate,
-            EndDate = dto.EndDate,
-            totalPrice = calculatedValue,
-            };
-
-            #region Email section
-            var r = "sohilaafify23@gmail.com";
-            var s = "Test subject";
-            var m = "Your Reservation is confirmed!";
-            await reservationsRepo.SendEmailAsync(r, s, m);
-            #endregion
-
-            await reservationsRepo.AddReservarion(caregiverPatientReservation);
-
-            return pricePerHour * totalDays;
-            return Ok(dto);
-        }
-
-        #endregion
-
-
-        #region  Confirm or reject or cancel reservation request
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateReservationStatusAsync(int id, [FromForm] ReservationStatusDto dto)
-        {
-            var reservation = await  reservationsRepo.GetReservationById(id);
-
-            if (reservation == null) return NotFound($"No reservation was found with ID: {id}");
-
-            if (dto.Status == ReservationStatus.Done)
-            {
-                reservation.Status = dto.Status;
-              if(reservationsRepo.CheckReservationDatesExists(reservation.OrderId)) { return BadRequest(); }
-              else { AddDatesToDatabase(reservation); }
-                   
-                
-            return Ok(dto);
-        }
-        
-        #endregion
-
-        #region Reserve a caregiver
-        [HttpPost]
-        public async Task<IActionResult> CreateReservationsAsync([FromForm] PostReservationDto  dto, [FromQuery] string CaregiverId)
-        {
             var caregiver = await _dbCaregiver.GetAsync(a => a.Id == CaregiverId);
             var pricePerDay = caregiver.PricePerDay;
             ReservationStatus reservationStatus = ReservationStatus.OnProgress;
@@ -467,9 +196,6 @@ namespace Caregiver.Controllers
         }
         #endregion
 
-        #region Add dates to database
-        protected async void AddDatesToDatabase(CaregiverPatientReservation reservation)
-        {
             var caregiverPatientReservation = new CaregiverPatientReservation
             {
             CaregiverId= CaregiverId,
@@ -489,15 +215,6 @@ namespace Caregiver.Controllers
 
             await reservationsRepo.AddReservarion(caregiverPatientReservation);
 
-            for (DateTime date = reservation.StartDate; date <= reservation.EndDate; date = date.AddDays(1))
-            {
-                ReservationDates reservationDate = new ReservationDates
-                {
-                    OrderId = reservation.OrderId,
-                    ReservationDate = date
-                };
-                await reservationsRepo.AddReservationDates(reservationDate);
-            }
             return Ok(dto);
         }
 
@@ -554,6 +271,70 @@ namespace Caregiver.Controllers
 
             return pricePerHour * totalDays;
         }
+        #endregion
+
+        #region Remove Reservation Dates
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveReservationDates(int id)
+        {
+            var reservation = await reservationsRepo.DeleteReservationStatus(id);
+            if (reservation == null) return NotFound($"No reservation was found with ID: {id}");
+            return Ok(reservation);
+
+        }
+        #endregion
+
+
+
+
+        #region in case i needed  this 
+        // var pricrPerDay = _context.Caregivers.Select(a => a.PricePerDay).FirstOrDefaultAsync(a => dto.CaregiverId == dto.CaregiverId);
+        // var loggedInUserId = _userManager.GetUserId(_httpContextAccessor.HttpContext.User);
+        // ReservationStatus ReservationStatus = ReservationStatus.OnProgress;
+        // var calculatedValue = CalculateTotalPrice(pricrPerDay);
+        //var CaregiverPatientReservation = new CaregiverPatientReservation
+        // {
+        //    PatientId= dto.PatientId,
+        // CaregiverId = dto.CaregiverId,
+        // StartDate = dto.StartDate,
+        //     EndDate = dto.EndDate,
+        //     totalPrice = dto.totalPrice,
+        // };
+        // CaregiverPatientReservation.PatientId = loggedInUserId;
+        // CaregiverPatientReservation.Status = ReservationStatus;
+
+        // CaregiverPatientReservation.totalPrice= calculatedValue;
+        // await _context.Reservations.AddAsync(CaregiverPatientReservation);
+        // _context.SaveChanges();
+        // return Ok(dto);
+
+
+        /*
+List<DateTime> generatedDates = new List<DateTime>();
+
+for (DateTime date = reservation.StartDate; date <= reservation.EndDate; date = date.AddDays(1))
+{
+    generatedDates.Add(date);
+}
+
+// Now you can save the generated dates to the ReservationDates table
+foreach (DateTime generatedDate in generatedDates)
+{
+    ReservationDates reservation1 = new ReservationDates
+    {
+        OrderId = reservation.OrderId,
+        ReservationDate = generatedDate
+    };
+
+    // Save the reservation to the database
+    _context.ReservationDates.Add(reservation1);
+}
+
+// Save changes to the database
+
+_context.SaveChanges();
+*/
+
         #endregion
 
         #region Remove Reservation Dates
