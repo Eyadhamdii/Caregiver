1f5d228559878b1f20a4c6c1a66a80b88ebad4ce
Eyad <Eyad@DESKTOP-LTMLQ70>
2024-03-25 06:55:33 +0200

    .

--- Caregiver/Models/CaregiverPatientReservation.cs
+++ Caregiver/Models/CaregiverPatientReservation.cs
@@ -1,24 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+using static Caregiver.Enums.Enums;
 
 namespace Caregiver.Models
 {
-	public class CaregiverPatientReservation
-	{
-		//
-		[Key]
+    public class CaregiverPatientReservation
+    {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OrderId { get; set; }
 
         [ForeignKey("Caregiver")]
-		public string CaregiverId { get; set; }
-		public CaregiverUser Caregiver { get; set; }
-		//
+        public string CaregiverId { get; set; }
+        public CaregiverUser Caregiver { get; set; }
 
-		[ForeignKey("Patient")]
-		public string PatientId { get; set; }
-		public PatientUser Patient { get; set; }
-		public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
 
-	}
+        [ForeignKey("Patient")]
+        public string PatientId { get; set; }
+        public PatientUser Patient { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime EndDate { get; set; }
+
+        public ReservationStatus Status { get; set; }
+
+        public int totalPrice { get; set; }
+
+
+
+    }
 }
